25c02f67692e125e588ba396fb580ff547352fa7
Wits-Q <witsjob@gmail.com>
2023-01-24 21:28:43 -0300

    segunda_etapa

---
+++ Tarefas/src/Tarefas.Web/Models/TarefasViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarefas.Web.Models
+{
+    public class TarefasViewModel
+    {
+        [DisplayName(Titulo)]
+        public string Titulo {get;set;}
+        public string Descricao {get;set;}
+    }
+}
